2c6a3567f17ee8db398285079aec4a3fffef5b56
Vladas <vladas.budvytis@gmail.com>
2023-07-27 17:11:22 +0300

    Other project files

--- part_dict.py
+++ part_dict.py
@@ -1,24 +1,41 @@
 PC_PARTS_DICT = {
-    "CPU" : {
+    "CPU": {
         "AMD Ryzen 5 5600X": {
-            "Core_count": 6, "Performace_speed": 3.7, "Boost_speed": 4.6, "TDP": 65
-        }, 
+            "Core_count": 6,
+            "Performace_speed": 3.7,
+            "Boost_speed": 4.6,
+            "TDP": 65,
+        },
         "AMD Ryzen 7 5800X": {
-            "Core_count": 8, "Performace_speed": 3.8, "Boost_speed": 4.7, "TDP": 105,
+            "Core_count": 8,
+            "Performace_speed": 3.8,
+            "Boost_speed": 4.7,
+            "TDP": 105,
         },
         "Intel Core i7-13700K": {
-            "Core_count": 16, "Performace_speed": 3.4, "Boost_speed": 5.4, "TDP": 25
+            "Core_count": 16,
+            "Performace_speed": 3.4,
+            "Boost_speed": 5.4,
+            "TDP": 25,
         },
         "AMD Ryzen 7 7800X3D": {
-            "Core_count": 8, "Performace_speed": 4.2, "Boost_speed": 5, "TDP": 20
-        }, 
+            "Core_count": 8,
+            "Performace_speed": 4.2,
+            "Boost_speed": 5,
+            "TDP": 20,
+        },
         "Intel Core i9-13900K": {
-            "Core_count": 24, "Performace_speed": 3, "Boost_speed": 5.8, "TDP": 25
+            "Core_count": 24,
+            "Performace_speed": 3,
+            "Boost_speed": 5.8,
+            "TDP": 25,
         },
     },
-    "Cooler" : {
-        "Cooler Master Hyper 212 Black Edition" : {
-            "Fan_RPM": "800 - 2000 RPM", "Noise_level" : "6.5-26 dB", "Color" : "Unknovn"
+    "Cooler": {
+        "Cooler Master Hyper 212 Black Edition": {
+            "Fan_RPM": "800 - 2000 RPM",
+            "Noise_level": "6.5-26 dB",
+            "Color": "Unknovn",
         },
     },
 }
